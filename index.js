7a56502d5d53c1bf63872ade9d7cd2dc6e200605
Kirstyn Canull <kirstyncanull@kirstyns-air.mynetworksettings.com>
2024-01-11 21:51:15 -0500

    submitting fetch lab

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(response => response.json())
+  .then(books => {
+    // const h1 = document.createElement("h1");
+    // h1.textContent = books[5].name;
+    // console.log(books[5]);
+    // document.body.append(h1);
+    renderBooks(books);
+})
+.catch(error => {
+  console.log(error);
+  });
   
 }
 
